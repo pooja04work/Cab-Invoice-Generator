523782f2e18dc725c6af04074131d5ddd1742217
Pooja <pooja.14206@gmail.com>
2021-05-12 22:27:01 +0530

    added a new file

---
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class CabInvoiceGenerator {
+//    private static final double COST_PER_KILOMETER = 10;
+//    private static final int COST_PER_MINUTE = 1;
+//    private double MINIMUM_FARE = 5d;
+//
+//    public double calculateFare(double distance, int time) {
+//        double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
+//        return Math.max(totalFare, MINIMUM_FARE);
+//    }
+}
---
+++ src/test/java/com/bridgelabz/CabInvoiceServiceTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInvoiceServiceTest {
+//    @Test
+//    public void givenDistanceAndTime_ShouldReturnTotalFare(){
+//        CabInvoiceGenerator cabinvoiceGenerator = new CabInvoiceGenerator();
+//        double distance = 2.0;
+//        int time = 5;
+//        double fare = cabinvoiceGenerator.calculateFare(distance, time);
+//        Assert.assertEquals(25, fare, 0.0);
+//    }
+//
+//    @Test
+//    public void givenDistanceAndTime_ShouldReturnMinimumFare(){
+//        CabInvoiceGenerator cabinvoiceGenerator = new CabInvoiceGenerator();
+//        double distance = 0.1;
+//        int time = 1;
+//        double fare = cabinvoiceGenerator.calculateFare(distance, time);
+//        Assert.assertEquals(5, fare, 0.0);
+//    }
+}
